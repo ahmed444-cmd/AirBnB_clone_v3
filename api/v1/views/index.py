077a87278ee2c5a83a7ff76cfb5882579b618f86
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-04 14:35:28 -0700

    Mandatory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Set up and create initial files"""
-from flask import jsonify, make_response
-from api.v1.views import app_views
+""" Import """
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
-
-classes = {"Amenity": "amenities",
-           "City": "cities",
-           "Place": "places",
-           "Review": "reviews",
-           "State": "states",
-           "User": "users"}
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
-def view_status():
-    """Return a JSON object"""
-    response = jsonify({"status": "OK"})
-    response.headers["Content-Type"] = "application/json"
-    return response
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ API status """
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def storage_stats():
-    """Provide a JSON response"""
-    dict = {}
-    for cls, name in classes.items():
-        dict.update({name: storage.count(cls)})
-    response = jsonify(dict)
-    response.headers["Content-Type"] = "application/json"
-    return response
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieve the count of each object by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
