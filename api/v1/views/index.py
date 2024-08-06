8774cd6d5c99528562ee01715bbb7077e6595bad
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-06 12:24:17 -0700

    Mandaory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""
-This module houses the status of endpoints (routes).
-"""
-from models import storage
-from flask import Flask
+""" Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
-    Returns a status as a JSON object.
-    """
-    return jsonify({"status": "OK"})
+    """ Returns a status as a JSON object. """
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Acquires the object count based on type.
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Acquires the object count based on type. """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
