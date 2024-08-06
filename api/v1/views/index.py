a21268f889bd9953ee10bd7ede55abf27fe70d67
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-06 13:00:21 -0700

    Mandaory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-""" Index """
+"""Prepare Flask functions."""
+from flask import jsonify, make_response
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
 
+classes = {"Amenity": "amenities",
+           "City": "cities",
+           "Place": "places",
+           "Review": "reviews",
+           "State": "states",
+           "User": "users"}
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Returns a status as a JSON object. """
-    return jsonify(status="OK")
+
+@app_views.route('/status', strict_slashes=False)
+def view_status():
+    """Provides a JSON response."""
+    response = jsonify({"status": "OK"})
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """ Acquires the object count based on type. """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+@app_views.route('/stats', strict_slashes=False)
+def storage_stats():
+    """Provides a JSON response."""
+    dict = {}
+    for cls, name in classes.items():
+        dict.update({name: storage.count(cls)})
+    response = jsonify(dict)
+    response.headers["Content-Type"] = "application/json"
+    return response
